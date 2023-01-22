0fa038c297fe1aadc35f4fc7b06ba487662a1ce1
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-01-22 13:37:25 +0900

    impl token

---
+++ parser/src/parse/token.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Token {
+    Illegal,
+    EOF,  // ファイルの終端
+    Number(i32),
+    Plus,
+    Minus,
+    Slash,
+    Asterisk
+}
+impl Token {
+    pub fn get_precedence(&self) -> u8 {
+        match self {
+            Token::Plus | Token::Minus => 1,
+            Token::Asterisk | Token::Slash => 2,
+            _ => 0,
+        }
+    }
+}
